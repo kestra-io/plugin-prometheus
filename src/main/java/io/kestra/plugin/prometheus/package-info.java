c391792b077e409660d28b26e5c21417ad1db722
ben8t <pimpaudben@gmail.com>
2025-12-30 15:41:19 +0100

    chore: update plugin categories

--- src/main/java/io/kestra/plugin/prometheus/package-info.java
+++ src/main/java/io/kestra/plugin/prometheus/package-info.java
@@ -1,7 +1,11 @@
 @PluginSubGroup(
     title = "Prometheus",
     description = "Tasks to interact with Prometheus for monitoring, metrics, and observability.",
-    categories = PluginSubGroup.PluginCategory.TOOL
+    categories = PluginSubGroup.PluginCategory.TOOL,
+    categories = {
+        PluginSubGroup.PluginCategory.DATA,
+        PluginSubGroup.PluginCategory.INFRASTRUCTURE
+    }
 )
 package io.kestra.plugin.prometheus;
 

432e13616cf7ceff39a447b37d4d5c4aaeff78c1
Malay Dewangan <malaydewangan310@gmail.com>
2026-04-20 17:02:13 +0530

    fix: v2 compatibility

--- src/test/java/io/kestra/plugin/prometheus/TriggerTest.java
+++ src/test/java/io/kestra/plugin/prometheus/TriggerTest.java
@@ -9,12 +9,25 @@ import org.junit.jupiter.api.Test;
 import io.kestra.core.junit.annotations.EvaluateTrigger;
 import io.kestra.core.junit.annotations.KestraTest;
 import io.kestra.core.models.executions.Execution;
+=======
+import io.kestra.core.queues.DispatchQueueInterface;
+import io.kestra.core.repositories.LocalFlowRepositoryLoader;
+import io.kestra.core.runners.FlowListeners;
+import io.kestra.core.utils.IdUtils;
+import io.kestra.jdbc.runner.JdbcScheduler;
+import io.kestra.scheduler.AbstractScheduler;
+import io.kestra.worker.DefaultWorker;
+
+import io.micronaut.context.ApplicationContext;
+import jakarta.inject.Inject;
+>>>>>>> 63b0fe8 (fix: v2 compatibility)
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
 
 @KestraTest
 public class TriggerTest {
+<<<<<<< HEAD
 
     @SuppressWarnings("unchecked")
     @Test
@@ -29,4 +42,34 @@ public class TriggerTest {
         assertThat(metrics.getFirst().get("value"), is("150"));
         assertThat(metrics.getFirst().get("labels").toString(), containsString("trigger_test"));
     }
+<<<<<<< HEAD
+=======
+
+    protected Execution triggerFlow() throws Exception {
+        CountDownLatch queueCount = new CountDownLatch(1);
+        AtomicReference<Execution> lastExecution = new AtomicReference<>();
+
+        try (
+            AbstractScheduler scheduler = new JdbcScheduler(applicationContext, flowListenersService);
+            DefaultWorker worker = applicationContext.createBean(DefaultWorker.class, IdUtils.create(), 8, null)
+        ) {
+            executionQueue.addListener(execution ->
+            {
+                lastExecution.set(execution);
+                queueCount.countDown();
+                assertThat(execution.getFlowId(), is("prometheus-listen"));
+            });
+
+            worker.run();
+            scheduler.run();
+
+            localFlowRepositoryLoader.load(Objects.requireNonNull(this.getClass().getClassLoader().getResource("flows/prometheus-listen.yml")));
+
+            boolean await = queueCount.await(1, TimeUnit.MINUTES);
+            assertThat(await, is(true));
+
+            return lastExecution.get();
+        }
+    }
+>>>>>>> 63b0fe8 (fix: v2 compatibility)
 }
